9982f340c612d9d80492c22e727240dbe8beb615
Alessandro Cocilova <alessandro.cocilov@gmail.com>
2016-04-27 22:12:03 +0200

    Refactored all the previously made functions (eventually deleting the ones that becamed useless), considering the new concept of chessboard as a matrix of pointer to pieces structs

--- chesso.c
+++ chesso.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 #include "chesso.h"
 
 int main()
 {
 	char nextMove[10];
-	for (int i = 0; i<8; i++){
-		playerPawnHasMoved[i] = false;
-		cpuPawnHasMoved[i] = false;
-	}
+
 	printf("Welcome to Chesso, by Alessandro Cocilova \n");
 	printf("Please type your move in the format: StartcolStartrow.EndcolEndrow (es. a1.a2) \n\n\n");
 	initChessboard();
-	initPieces();
 	while (!playerHasWon()) { // senseless to pass the chessboard to this function, because there's only one chessboard
  	printChessboard();
  	do {
@@ -25,129 +22,134 @@ int main()
 
 };
 
-/*Inits the View side of the checkerboard*/
+/*Inits the checkerboard, allocating space for 20 structs, one for each piece in game, initializing them and making the corresponding squares target them*/
 void initChessboard(){
-	strcpy(row0, "  a b c d e f g h");
-	strcpy(col0, "12345678");
-	strcpy(matchChessboard[7], "-+-+r+c+");
-	strcpy(matchChessboard[6], "dddddddd");
-	strcpy(matchChessboard[5], "-+-+-+-+");
-	strcpy(matchChessboard[4], "+-+-+-+-");
-	strcpy(matchChessboard[3], "-+-+-+-+");
-	strcpy(matchChessboard[2], "+-+-+-+-");
-	strcpy(matchChessboard[1], "pppppppp");
-	strcpy(matchChessboard[0], "+-+-k-h-");
-};
-
-/*Inits the Controller side of the checkerboard*/
-void initPieces(){
+	/*Every empty square should be a nil pointer*/
 	for (int i = 0; i < 8; i++){
-		gamePieces[i].position[0] = 1;
-		gamePieces[i].position[1] = i;
-		gamePieces[i].myPieceType = PAWN;
-		gamePieces[i].color = WHITE;
-		gamePieces[i].avaiableForEnpassant = false;
+		for (int j = 0; j < 8; j++){
+			chessboard[i][j] = NULL;
+		}
 	}
-	gamePieces[8].position[0] = 0;
-	gamePieces[8].position[1] = 6;
-	gamePieces[8].myPieceType = KNIGHT;
-	gamePieces[8].color = WHITE;
-	gamePieces[8].avaiableForEnpassant = false;
 
-	gamePieces[9].position[0] = 0;
-	gamePieces[9].position[1] = 4;
-	gamePieces[9].myPieceType = KING;
-	gamePieces[9].color = WHITE;
-	gamePieces[9].avaiableForEnpassant = false;
+	for (int i = 0; i < 8; i++){
+		chessboard[1][i] = malloc(sizeof(piece));
 
-	for (int i = 10; i < 18; i++){
-		gamePieces[i].position[0] = 6;
-		gamePieces[i].position[1] = i;
-		gamePieces[i].myPieceType = PAWN;
-		gamePieces[i].color = BLACK;
-		gamePieces[i].avaiableForEnpassant = false;
+		chessboard[1][i]->myPieceType = PAWN;
+		chessboard[1][i]->color = WHITE;
+		chessboard[1][i]->avaiableForEnpassant = false;
 	}
-	gamePieces[18].position[0] = 7;
-	gamePieces[18].position[1] = 6;
-	gamePieces[18].myPieceType = KNIGHT;
-	gamePieces[18].color = BLACK;
-	gamePieces[18].avaiableForEnpassant = false;
 
-	gamePieces[19].position[0] = 7;
-	gamePieces[19].position[1] = 4;
-	gamePieces[19].myPieceType = KING;
-	gamePieces[19].color = BLACK;
-	gamePieces[19].avaiableForEnpassant = false;
+	chessboard[0][6] = malloc(sizeof(piece));
+
+	chessboard[0][6]->myPieceType = KNIGHT;
+	chessboard[0][6]->color = WHITE;
+	chessboard[0][6]->avaiableForEnpassant = false;
+
+	chessboard[0][4] = malloc(sizeof(piece));
+
+	chessboard[0][4]->myPieceType = KING;
+	chessboard[0][4]->color = WHITE;
+	chessboard[0][4]->avaiableForEnpassant = false;
+
+	for (int i = 0; i < 8; i++){
+		chessboard[6][i] = malloc(sizeof(piece));
+
+		chessboard[6][i]->myPieceType = PAWN;
+		chessboard[6][i]->color = BLACK;
+		chessboard[6][i]->avaiableForEnpassant = false;
+	}
+
+	chessboard[7][6] = malloc(sizeof(piece));
+
+	chessboard[7][6]->myPieceType = KNIGHT;
+	chessboard[7][6]->color = BLACK;
+	chessboard[7][6]->avaiableForEnpassant = false;
+
+	chessboard[7][4] = malloc(sizeof(piece));
+
+	chessboard[7][4]->myPieceType = KING;
+	chessboard[7][4]->color = BLACK;
+	chessboard[7][4]->avaiableForEnpassant = false;
+
+	/*Where i've to do the free()? When the games ends?*/
+
+	/*Consider refactoring*/
+	for (int i = 0; i<8; i++){
+		playerPawnHasMoved[i] = false;
+		cpuPawnHasMoved[i] = false;
+	}
 };
 
 void printChessboard(){
-
-	for (int i = 7; i >= 0; i--){ // because row8 has to be printfed first
-		printf("%c", col0[i]);
+	for (int i = 7; i>= 0; i--){
+		printf("%d ", i+1);
 		for (int j = 0; j < 8; j++){
-			printf(" %c", matchChessboard[i][j]);
+
+			/*Empty square*/
+			if (chessboard[i][j] == NULL) {
+				if ( (i+1)%2 == 0){/*Pairwise row*/
+					if ( (j+1)%2 == 0){/*Pairwise column*/
+						printf("+ ");
+					}
+					else {
+						printf("- ");
+					}
+				}
+				else {
+					if ( (j+1)%2 == 0){/*Pairwise column*/
+						printf("- ");
+					}
+					else {
+						printf("+ ");
+					}
+				}
+			}
+
+			/*Square occupied by a Pawn*/
+			else if (chessboard[i][j]->myPieceType == PAWN) {
+				if (chessboard[i][j]->color == WHITE) {
+					printf("p ");
+				} else {
+					printf("d ");
+				}
+			}
+
+			/*Square occupied by a Knight*/
+			else if (chessboard[i][j]->myPieceType == KNIGHT) {
+				if (chessboard[i][j]->color == WHITE) {
+					printf("h ");
+				} else {
+					printf("c ");
+				}
+			}
+
+			/*Square occupied by a King*/
+			else if (chessboard[i][j]->myPieceType == KING) {
+				if (chessboard[i][j]->color == WHITE) {
+					printf("k ");
+				} else {
+					printf("r ");
+				}
+			}
+
 		}
 		printf("\n");
 	}
-	printf("%s\n", row0); 
-
-};
-
-int findPieceToBeMoved(){
-	if (isPlayerTurn) {/*I search in the white pieces*/
-		for (int i = 0; i <10; i++) {
-			/*Because in lastMove pos is memorized col row, in .position is row.col*/
-			if ( (gamePieces[i].position[0] == lastMove[1]) & (gamePieces[i].position[1] == lastMove[0]) ) return i;
-		}
-	} 
-
-	else {
-		for (int i = 10; i <20; i++) {
-			if ( (gamePieces[i].position[0] == lastMove[1]) & (gamePieces[i].position[1] == lastMove[0]) ) return i;
-		}
-	}
-	return -1;
+	printf("  a b c d e f g h\n");
 };
 
 /*Moves a piece from its square to the specified square updating the model*/
 void move(char playerMove[]){
 	/*playerMove used just to provide a feedback to the player of CPU's move*/
 	printf("%s\n", playerMove);
-	int pieceToBeMoved;
-	pieceToBeMoved = findPieceToBeMoved();
 
-	if (pieceToBeMoved != -1) {
-		gamePieces[pieceToBeMoved].position[0] = lastMove[3];
-		gamePieces[pieceToBeMoved].position[1] = lastMove[2];
-		
-		printf("New position %d %d\n", gamePieces[pieceToBeMoved].position[0], gamePieces[pieceToBeMoved].position[1]);
-		if (gamePieces[pieceToBeMoved].myPieceType == PAWN) printf("you moved a pawn, didn't you?\n");
+	if (chessboard[lastMove[0]][lastMove[1]] != NULL) {
+		/*The square where i've moved the piece, now targets to the piece*/
+		chessboard[lastMove[2]][lastMove[3]] = chessboard[lastMove[0]][lastMove[1]];
+		/*Old square contains now a pointer to null, meaning that it has no more pieces*/
+		chessboard[lastMove[0]][lastMove[1]] = NULL;
 	}
-	else perror("Cannot find the piece!");
-	
-	updateChessboard();
-};
-
-/*Updates the visual side of the checkerboard. Fixes the empty squares after the moves*/
-void updateChessboard(){
-	matchChessboard[lastMove[3]][lastMove[2]] = matchChessboard[lastMove[1]][lastMove[0]];
-
-	if ( (lastMove[1]+1) % 2 == 1){
-		if ( (lastMove[0]+1) % 2 == 1){
-			matchChessboard[lastMove[1]][lastMove[0]] = '+';
-		}
-		else{
-			matchChessboard[lastMove[1]][lastMove[0]] = '-';
-		}
-	}
-	else{
-		if ( (lastMove[0]+1) % 2 == 1){
-			matchChessboard[lastMove[1]][lastMove[0]] = '-';
-		}
-		else{
-			matchChessboard[lastMove[1]][lastMove[0]] = '+';
-		}
-	}
+	else perror("The selected square is empty");
 };
 
 /*Translates the player move from standard chess ('a1b1') form to numeric form*/
@@ -177,90 +179,69 @@ int* decodePlayerMove(char playerMove[]){
 	endRow = playerMove[4] - '0' -1;
 
 	/*I'm ignoring the third char in the string, since it contains the dot*/
-	lastMove[0] = startCol;
-	lastMove[1] = startRow;
-	lastMove[2] = endCol;
-	lastMove[3] = endRow;
+	/*Memorize them in row, col format!*/
+	lastMove[0] = startRow;
+	lastMove[1] = startCol;
+	lastMove[2] = endRow;
+	lastMove[3] = endCol;
 	//printf("Dentro decode %d %d %d %d\n", startCol, startRow, endCol, endRow);
 
 	return lastMove;
 }
 
 /*Checks if the passed move is legal. All other functions use global variable 'lastMove'. More elegant to make it uniform?*/
-bool moveIsLegal(int playerMoveDecoded[]){
-	//printf("Dentro moveIsLegal %d %d %d %d\n", playerMoveDecoded[0], playerMoveDecoded[1], playerMoveDecoded[2], playerMoveDecoded[3]);
+bool moveIsLegal(int move[]){
+
 	/*Piece not in the board limits*/
-	if (playerMoveDecoded[0] < 0 | playerMoveDecoded[0] > 8 | playerMoveDecoded[1] < 0 | playerMoveDecoded[1] > 8 | playerMoveDecoded[2] < 0 | playerMoveDecoded[2] > 8 | playerMoveDecoded[3] < 0 | playerMoveDecoded[3] > 8) {
+	if (move[0] < 0 | move[0] > 8 | move[1] < 0 | move[1] > 8 | move[2] < 0 | move[2] > 8 | move[3] < 0 | move[3] > 8) {
 		if (isPlayerTurn) printf("Illegal move: specified move outside board limits\n");
 		return 0;
 	}
 	/*Trying to move from and empty square. In the matrix order is row-col, not col-row, so the order is inverted*/
-	if ( (matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='+') | (matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='-') ) {
+	if ( chessboard[move[0]][move[1]] == NULL ) {
 		if (isPlayerTurn) printf("Illegal move: trying to move an empty square\n");
 		return 0;
 	}
 
 	/*Trying to move adversary's pieces*/
-	if ( ((matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='d') & isPlayerTurn) | 
-		 ((matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='c') & isPlayerTurn) | 
-		 ((matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='r') & isPlayerTurn) | 
-		 ((matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='p') & !isPlayerTurn) | 
-		 ((matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='h') & !isPlayerTurn) | 
-		 ((matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='k') & !isPlayerTurn) ) {
+	if ( ((chessboard[move[0]][move[1]]->color == BLACK) & isPlayerTurn) | 
+		 ((chessboard[move[0]][move[1]]->color == WHITE) & !isPlayerTurn) ) {
 		if (isPlayerTurn) printf("Illegal move: trying to move piece that belongs to the adversary\n");
 		return 0;
 	}
 
-	/*MUST BE REFACTORED CONSIDERING THE NEW DATA STRUCTURES!!!*/
-	
-	/*Pawn moves for player*/
-	if (matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='p') {
-		int tmp = playerMoveDecoded[0] - playerMoveDecoded[2];
+	if (chessboard[move[0]][move[1]]->myPieceType == PAWN){
 
-		if (playerMoveDecoded[0] == playerMoveDecoded[2]){
-			if (playerMoveDecoded[3] - playerMoveDecoded[1] == 1) {
-				playerPawnHasMoved[playerMoveDecoded[0]] = true;
-				return 1;
+		if (chessboard[move[0]][move[1]]->color == WHITE){
+			int tmp = move[1] - move[3];
+			/*Single step move*/
+			if (move[1] == move[3]){
+				/*Pawn that goes on in straight line, cannot eat another piece*/
+				if (chessboard[move[2]][move[3]] == NULL){
+						if (move[2] - move[0] == 1) {
+						playerPawnHasMoved[move[1]] = true;
+						return 1;
+					}
+					/*Double step move*/
+					else if ( (move[2] - move[0] == 2) & !playerPawnHasMoved[move[1]] & move[0] == 1) { /*added pawn's line check, otherwise a pawn that has changed his row eating, could use another's pawn +2 bonus*/
+						playerPawnHasMoved[move[1]] = true;
+						return 1;
+					}
+				}
 			}
-			else if ( (playerMoveDecoded[3] - playerMoveDecoded[1] == 2) & !playerPawnHasMoved[playerMoveDecoded[0]] & playerMoveDecoded[1] == 1) { /*added, otherwise a pawn that has changed his row eating, could use another's pawn +2 bonus*/
-				playerPawnHasMoved[playerMoveDecoded[0]] = true;
-				return 1;
+			/*If I move to a next column, legit only if i'm eating an adversary's piece*/
+			else if (tmp*tmp == 1){
+				if (chessboard[move[2]][move[3]]->color == BLACK) {
+					playerPawnHasMoved[move[1]] = true;
+					return 1;
+				}
 			}
+			/*EN PASSANT TO BE IMPLENTED!*/
 		}
-		/*If I move to a next column*/
-		else if (tmp*tmp == 1){
-			if ( (matchChessboard[playerMoveDecoded[3]][playerMoveDecoded[2]] =='d') | (matchChessboard[playerMoveDecoded[3]][playerMoveDecoded[2]] =='c') | 
-				(matchChessboard[playerMoveDecoded[3]][playerMoveDecoded[2]] =='r') ) {
-				playerPawnHasMoved[playerMoveDecoded[0]] = true;
-				return 1;
-			}
-		}
-		/*EN PASSANT TO BE IMPLENTED!*/
-	}
 
-	/*Pawn moves for cpu*/
-	if (matchChessboard[playerMoveDecoded[1]][playerMoveDecoded[0]] =='d') {
-		int tmp = playerMoveDecoded[0] - playerMoveDecoded[2];
+		/*IF COLOR = BLACK*/
 
-		if (playerMoveDecoded[0] == playerMoveDecoded[2]){
-			if (playerMoveDecoded[3] - playerMoveDecoded[1] == 1) {
-				playerPawnHasMoved[playerMoveDecoded[0]] = true;
-				return 1;
-			}
-			else if ( (playerMoveDecoded[3] - playerMoveDecoded[1] == 2) & !playerPawnHasMoved[playerMoveDecoded[0]] & playerMoveDecoded[1] == 1) {
-				playerPawnHasMoved[playerMoveDecoded[0]] = true;
-				return 1;
-			}
-		}
-		/*If I move to a next column*/
-		else if (tmp*tmp == 1){
-			if ( (matchChessboard[playerMoveDecoded[3]][playerMoveDecoded[2]] =='p') | (matchChessboard[playerMoveDecoded[3]][playerMoveDecoded[2]] =='h') | 
-				(matchChessboard[playerMoveDecoded[3]][playerMoveDecoded[2]] =='k') ) {
-				playerPawnHasMoved[playerMoveDecoded[0]] = true;
-				return 1;
-			}
-		}
-		/*EN PASSANT TO BE IMPLENTED!*/
+
 	}
 
 	return 0;
--- chesso.h
+++ chesso.h
@@ -1,5 +1,3 @@
-char matchChessboard[8][9];
-
 typedef enum {PAWN, KNIGHT, KING} pieceType;
 
 typedef enum {BLACK, WHITE} pieceColor;
@@ -7,42 +5,36 @@ typedef enum {BLACK, WHITE} pieceColor;
 typedef struct {
 	pieceType myPieceType;
 	pieceColor color;
-	int position[2]; //row, col. set to -1, -1 if piece has been eaten
+	//int position[2]; //row, col. set to -1, -1 if piece has been eaten
 	bool avaiableForEnpassant;
 } piece;
 
-piece gamePieces[20];
+piece* chessboard[8][8];
 
 // more elegant integrate it into the struct?
 bool playerPawnHasMoved[8];
 
 bool cpuPawnHasMoved[8];
 
-
 char row0[18]; 
 
 char col0[9];
 
+/*Global var, mantains the last move actually tried by the player that moved last*/
 int lastMove[4];
 
 bool isPlayerTurn = true; /*when false is CPU's turn*/
 
 void initChessboard();
 
-void initPieces();
-
 void printChessboard();
 
 bool playerHasWon();
 
-int findPieceToBeMoved();
-
 void move(char playerMove[]);
-
-void updateChessboard();
 
 int* decodePlayerMove(char playerMove[]);
 
-bool moveIsLegal(int playerMoveDecoded[]);
+bool moveIsLegal(int move[]);
 
 bool playerHasWon();
